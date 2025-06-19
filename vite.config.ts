4f0a00e851bcf9432c20d2c9f9b63cc02696597e
Diego Marrufo <yojouhanshumi@gmail.com>
2025-06-19 22:19:10 +0200

    added vitest coverage and coverage ui

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,20 @@
 /// <reference types="vitest" />
 
+import { configDefaults } from 'vitest/config'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
 	test: {
-		globals: true
+		globals: true,
+		coverage: {
+			provider: 'v8',
+			enabled: true,
+			exclude: [
+				...configDefaults.exclude, 
+				'main.js',
+				'version-bump.mjs',
+				"**/*.config.*"
+			]
+		}
 	}
 })
